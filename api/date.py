c41b39d1fc8a78edd885bc1022e9bcb9c1a1c17e
GhaidaMomani <ghaidaalmomani3@gmail.com>
2022-04-11 23:35:34 +0200

    init

--- api/date.py
+++ api/date.py
@@ -30,7 +30,7 @@ class handler(BaseHTTPRequestHandler):
 
 
 
-  def do_GET(self):
+#   def do_GET(self):
     # self.send_response(200)
     # self.send_header('Content-type', 'text/plain')
     # self.end_headers()
