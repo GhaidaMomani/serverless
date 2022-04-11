79cdf954e9a790c9b301f72fbe7fbbc43c60509a
GhaidaMomani <ghaidaalmomani3@gmail.com>
2022-04-12 00:46:03 +0200

    init

--- api/rename.py
+++ api/rename.py
@@ -3,29 +3,30 @@ from urllib import parse
 import requests
 
 class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        s = self.path
-        url_components = parse.urlsplit(s)
-        query_string_list = parse.parse_qsl(url_components.query)
-        dic = dict(query_string_list)
+  
+  def do_GET(self):
+    url_path = self.path
+    url_components = parse.urlsplit(url_path)
+    query_string_list = parse.parse_qsl(url_components.query)
+    dic = dict(query_string_list)
+    if 'platform' in dic:
+      url = 'https://www.gamerpower.com/api/giveaways'
+      url = url + '?platform=' + dic['platform']
+      r = requests.get(url) 
+  
+      data = r.json()
+      games = []
+      for n in range(len(data)):
+        giveaways = {'platform': data[n]['platforms'], 'title': data[n]['title'], 'url': data[n]['open_giveaway_url'], 'status': data[n]['status'] }
+        games.append(giveaways)
+        message = str(games)
+        
+    else:
+      message = ('Please provide a valid platform')
+  
+    self.send_response(200)
+    self.send_header('Content-type', 'text/plain')
+    self.end_headers()
+    self.wfile.write(message.encode())
 
-        if "word" in dic:
-            url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
-            r = requests.get(url + dic["word"])
-            data = r.json()
-            definitions = []
-            for word_data in data:
-                definition = word_data["meanings"][0]["definitions"][0]["definition"]
-                definitions.append(definition)
-            message = str(definitions)
-
-        else:
-            message = "Give me a word to define please"
-
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-
-        self.wfile.write(message.encode())
-
-        return
+    return
