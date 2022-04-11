a280c9f2dc90b2a7e0eb0a708ed597e8d7223701
GhaidaMomani <ghaidaalmomani3@gmail.com>
2022-04-11 23:50:52 +0200

    init

--- api/date.py
+++ api/date.py
@@ -36,5 +36,6 @@ class handler(BaseHTTPRequestHandler):
     # self.send_header('Content-type', 'text/plain')
     # self.end_headers()
     # self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
+    
     return
  
